982ce0dea54b233aee957a47e83df9c6a08be157
kito0 <drewshah0@gmail.com>
2021-11-09 23:13:35 -0700

    Resolve warnings

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
+const socket = io('https://kito-video-chat.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
 	const [stream, setStream] = useState();
