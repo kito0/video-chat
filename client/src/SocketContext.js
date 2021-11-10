9e274c21540c72c29ca2b5bc8af333c4f5b15037
kito0 <drewshah0@gmail.com>
2021-11-09 23:08:16 -0700

    Update SocketContext

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -7,7 +7,7 @@ const SocketContext = createContext();
 const socket = io('http://localhost:5000');
 
 const ContextProvider = ({ children }) => {
-	const [stream, setStream] = useState(null);
+	const [stream, setStream] = useState();
 	const [me, setMe] = useState('');
 	const [call, setCall] = useState({});
 	const [callAccepted, setCallAccepted] = useState(false);
@@ -26,7 +26,7 @@ const ContextProvider = ({ children }) => {
 				myVideo.current.srcObject = currentStream;
 			});
 
-		socket.on('me', (id) => setImmediate(id));
+		socket.on('me', (id) => setMe(id));
 		socket.on('calluser', ({ from, name: callerName, signal }) => {
 			setCall({ isReceivedCall: true, from, name: callerName, signal });
 		});
@@ -54,7 +54,7 @@ const ContextProvider = ({ children }) => {
 		const peer = new Peer({ initiator: true, trickle: false, stream });
 
 		peer.on('signal', (data) => {
-			socket.emit('callUser', {
+			socket.emit('calluser', {
 				userToCall: id,
 				signalData: data,
 				from: me,
